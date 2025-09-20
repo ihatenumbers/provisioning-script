f31de3cea243efc5e553c59c156b69ed1cd064f1
bakanayatsu <78297228+ihatenumbers@users.noreply.github.com>
2025-09-20 17:02:46 +0800

    Update qwen-edit.sh

--- qwen-edit.sh
+++ qwen-edit.sh
@@ -34,6 +34,7 @@ DIFFUSION_MODELS=(
 
 LORA_MODELS=(
     "https://huggingface.co/Bakanayatsu/noobai-loras/resolve/main/anime-stripper.safetensors"
+    "https://huggingface.co/Bakanayatsu/noobai-loras/resolve/main/anime-stripper2.safetensors"
     "https://huggingface.co/lightx2v/Qwen-Image-Lightning/resolve/main/Qwen-Image-Edit-Lightning-4steps-V1.0-bf16.safetensors"
 )
 
